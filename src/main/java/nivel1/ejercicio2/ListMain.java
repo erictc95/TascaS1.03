efef89ac224909de0f101e271e6f12b24b654dbb
erictc95 <maxerictc95@hotmail.com>
2026-02-11 12:52:32 +0100

    Version 2 - show list 1, put the list 2 and show.

--- src/main/java/nivel1/ejercicio2/ListMain.java
+++ src/main/java/nivel1/ejercicio2/ListMain.java
@@ -1,7 +1,6 @@
 package nivel1.ejercicio2;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.ListIterator;
 
 public class ListMain {
@@ -16,15 +15,21 @@ public class ListMain {
         it1.add(3);
         it1.add(4);
 
-        Iterator<Integer> it = list1.iterator();
+        System.out.println("Lista: ");
+        ListIterator<Integer> it = list1.listIterator();
+        while (it.hasNext()) {
+            Integer num = it.next();
+            System.out.println(num);
+            it2 = list2.listIterator(0); // cursor al inicio de list2
+            it2.add(num); // inserta al inicio
+        }
+
+        System.out.println("Lista invertida: ");
+        it = list2.listIterator();
         while (it.hasNext()) {
             Integer num = it.next();
             System.out.println(num);
         }
-
-
-
-
 
     }
 }
