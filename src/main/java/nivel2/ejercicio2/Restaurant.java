159ac5114ca4c83a9a064b688ce1e4bc01e44a22
erictc95 <maxerictc95@hotmail.com>
2026-02-12 13:20:22 +0100

    Version 1 - implements new method compareTo in Restaurant clss.

---
+++ src/main/java/nivel2/ejercicio2/Restaurant.java
@@ -0,0 +1,64 @@
+package nivel2.ejercicio2;
+
+
+import java.util.Objects;
+
+public class Restaurant implements Comparable<Restaurant> {
+    private String name;
+    private int score;
+
+    public Restaurant (String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    @Override
+    public String toString() {
+        return "Restaurant{" +
+                "name='" + name + '\'' +
+                ", score=" + score +
+                '}';
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, score);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (!(obj instanceof Restaurant)) {
+            return false;
+        }
+        Restaurant restaurant = (Restaurant) obj;
+        return Objects.equals(name, restaurant.name) && score == restaurant.score;
+    }
+
+    @Override
+    public int compareTo(Restaurant other) {
+        int nameCoparision = this.name.compareTo(other.name);
+        if (nameCoparision != 0) {
+            return nameCoparision;
+        }
+        return Integer.compare(other.score, this.score);
+    }
+}
---
+++ src/main/java/nivel2/ejercicio2/RestaurantsMain.java
@@ -0,0 +1,23 @@
+package nivel2.ejercicio2;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+
+public class RestaurantsMain {
+    public static void main(String[] args) {
+        HashSet<Restaurant> restaurants = new HashSet<>();
+
+        restaurants.add(new Restaurant("La Pampara", 5));
+        restaurants.add(new Restaurant("La Pampara", 7));
+        restaurants.add(new Restaurant("La Pampara", 5));
+        restaurants.add(new Restaurant("La Bodegueta", 9));
+        restaurants.add(new Restaurant("Volcanic", 8));
+
+        for (Restaurant r : restaurants) {
+            System.out.println(r);
+        }
+
+
+    }
+}
