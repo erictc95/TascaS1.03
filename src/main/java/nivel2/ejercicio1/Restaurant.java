6ef56439159adf1b964c361e01bff44c52f249b7
erictc95 <maxerictc95@hotmail.com>
2026-02-12 12:01:02 +0100

    Version 1 - Restaurant class with override methods to no repite the names.

---
+++ src/main/java/nivel2/ejercicio1/Restaurant.java
@@ -0,0 +1,53 @@
+package nivel2.ejercicio1;
+
+
+public class Restaurant {
+    private String name;
+    private int score;
+
+    public Restaurant (String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    @Override
+    public String toString() {
+        return "Restaurant{" +
+                "name='" + name + '\'' +
+                ", score=" + score +
+                '}';
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (!(obj instanceof Restaurant)) {
+            return false;
+        }
+        Restaurant restaurant = (Restaurant) obj;
+        return name.equals(restaurant.name);
+    }
+}
