161d395886369cd9eedc4cd9ada034626128b66d
erictc95 <maxerictc95@hotmail.com>
2026-02-12 13:33:58 +0100

    Version 3 - Cant add duplicates because is an ArrayList no hashmap.

--- src/main/java/nivel2/ejercicio2/RestaurantsMain.java
+++ src/main/java/nivel2/ejercicio2/RestaurantsMain.java
@@ -6,28 +6,16 @@ import java.util.HashSet;
 
 public class RestaurantsMain {
     public static void main(String[] args) {
-        HashSet<Restaurant> restaurants = new HashSet<>();
-
-        restaurants.add(new Restaurant("La Pampara", 5));
-        restaurants.add(new Restaurant("La Pampara", 7));
-        restaurants.add(new Restaurant("La Pampara", 5));
-        restaurants.add(new Restaurant("La Bodegueta", 9));
-        restaurants.add(new Restaurant("Volcanic", 8));
-
-        for (Restaurant r : restaurants) {
-            System.out.println(r);
-        }
 
         ArrayList<Restaurant> restaurantsList = new ArrayList<>();
         restaurantsList.add(new Restaurant("La Pampara", 5));
         restaurantsList.add(new Restaurant("La Pampara", 7));
         restaurantsList.add(new Restaurant("Volcanic", 8));
         restaurantsList.add(new Restaurant("La Bodegueta", 9));
-        restaurantsList.add(new Restaurant("Volcanic", 8));
 
         Collections.sort(restaurantsList);
 
-        for (Restaurant r : restaurants) {
+        for (Restaurant r : restaurantsList) {
             System.out.println(r);
         }
     }
