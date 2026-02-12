dabaff205d3ecb428cfcd755e154f47bf373865b
erictc95 <maxerictc95@hotmail.com>
2026-02-12 12:57:15 +0100

    Version 3 - No duplicates by the same name + score and add more restaurant examples.

---
+++ src/main/java/nivel2/ejercicio1/RestaurantsMain.java
@@ -0,0 +1,19 @@
+package nivel2.ejercicio1;
+
+import java.util.HashSet;
+
+public class RestaurantsMain {
+    public static void main(String[] args) {
+        HashSet<Restaurant> restaurants = new HashSet<>();
+
+        restaurants.add(new Restaurant("La Pampara", 5));
+        restaurants.add(new Restaurant("La Pampara", 7));
+        restaurants.add(new Restaurant("La Pampara", 5));
+        restaurants.add(new Restaurant("La Bodegueta", 9));
+        restaurants.add(new Restaurant("Volcanic", 8));
+
+        for (Restaurant r : restaurants) {
+            System.out.println(r);
+        }
+    }
+}
